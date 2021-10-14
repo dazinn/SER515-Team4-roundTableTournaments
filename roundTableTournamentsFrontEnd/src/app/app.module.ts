f714034e60725a5b33b7f96b8683f7ad6419084e
asidharth1122 <52005401+asidharth1122@users.noreply.github.com>
2021-10-14 11:25:06 -0700

    Login Page Front End
    Added a few changes to the login page

--- roundTableTournamentsFrontEnd/src/app/app.module.ts
+++ roundTableTournamentsFrontEnd/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    LoginComponent
+    LoginComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
