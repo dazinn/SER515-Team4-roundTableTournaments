51fddc5428649c76098a742e1c0baed91afc756d
dazinn <dazinn@asu.edu>
2021-11-24 01:23:55 -0700

    us57-t127: Added round robin scheduler package to project for use in generating the match schedule.

--- roundTableTournamentsFrontEnd/src/app/team-director/team-director.component.ts
+++ roundTableTournamentsFrontEnd/src/app/team-director/team-director.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var require: any
+
 @Component({
   selector: 'app-team-director',
   templateUrl: './team-director.component.html',
@@ -10,6 +12,21 @@ export class TeamDirectorComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.testRoundRobinScheduler();
+  }
+
+  // The round robin algorithm we are using is a package called 'roundrobin' installed via npm and saved as a dependecy 
+  // to the project. Its source can be found here: https://github.com/clux/roundrobin. The following function conducts 
+  // a simple test to verify the scheduler works. 
+  testRoundRobinScheduler() {
+    var robin = require('roundrobin');
+    let scheduleOutput = robin(6); // Tell the round robin algorithm we want to schedule 6 teams
+    console.log("First Match in schedule: " + scheduleOutput[0][0]); // Outputs first match (should be teams 1 and 6)
+  }
+
+  // TODO: access team array to get names of teams in tournament. Add them to array or list, then pass them to robin()?
+  generateSchedule() {
+    //TODO: implement
   }
 
 }
